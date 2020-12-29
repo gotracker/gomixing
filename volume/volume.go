e7e21c9c2ed70930bc3d536685580755b7a7cc61
Jason Crawford <jcrawford@dm78.org>
2020-12-28 16:25:35 -0800

    Add support for overflow protection

--- volume/volume.go
+++ volume/volume.go
@@ -7,6 +7,7 @@ type Volume float32
 
 var (
 	// VolumeUseInstVol tells the system to use the volume stored on the instrument
+	// This is useful for trackers and other musical applications
 	VolumeUseInstVol = Volume(math.Inf(-1))
 )
 
@@ -21,31 +22,33 @@ type Int24 struct {
 
 // ToSample returns a volume as a typed value supporting the bits per sample provided
 func (v Volume) ToSample(bitsPerSample int) interface{} {
+	val := v.withOverflowProtection()
 	switch bitsPerSample {
 	case 8:
-		return int8(v * 128.0)
+		return int8(val * 128.0)
 	case 16:
-		return int16(v * 32678.0)
+		return int16(val * 32678.0)
 	case 24:
-		s := int32(v * 8388608.0)
+		s := int32(val * 8388608.0)
 		return Int24{Hi: int8(s >> 16), Lo: uint16(s & 65535)}
 	case 32:
-		return int32(v * 2147483648.0)
+		return int32(val * 2147483648.0)
 	}
 	return 0
 }
 
 // ToIntSample returns a volume as an int32 value ranged to the bits per sample provided
 func (v Volume) ToIntSample(bitsPerSample int) int32 {
+	val := v.withOverflowProtection()
 	switch bitsPerSample {
 	case 8:
-		return int32(v * 128.0)
+		return int32(val * 128.0)
 	case 16:
-		return int32(v * 32678.0)
+		return int32(val * 32678.0)
 	case 24:
-		return int32(v * 8388608.0)
+		return int32(val * 8388608.0)
 	case 32:
-		return int32(v * 2147483648.0)
+		return int32(val * 2147483648.0)
 	}
 	return 0
 }
@@ -68,4 +71,17 @@ func (m Matrix) Apply(samp ...Volume) Matrix {
 		}
 	}
 	return o
+}
+
+func (v Volume) withOverflowProtection() float64 {
+	val := float64(v)
+	if math.Abs(val) <= 1.0 {
+		// likely case
+		return val
+	} else if math.Signbit(val) {
+		// overflow, negative
+		return -1.0
+	}
+	// overflow, positive
+	return 1.0
 }
